a606b215e13486a47500c76074fd0f09aecd8131
tjdwhd70599@gmail.com <tjdwhd70599@gmail.com>
2022-05-29 05:35:35 +0900

    SpringBoot

---
+++ login/src/main/java/com/example/springboot/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.springboot.service;
+
+import com.example.springboot.model.User;
+import com.example.springboot.web.dto.UserRegistrationDto;
+
+public interface UserService {
+	User save(UserRegistrationDto registrationDto);
+}
